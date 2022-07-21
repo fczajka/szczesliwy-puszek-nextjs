65a1a626600bcf9fe08023d208c3278711fd6081
FranciszekCzajka <franekczajkaa@gmail.com>
2022-07-21 21:39:37 +0200

    Fix crossorigin -> crossOrigin='true'

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
                     <link
                         rel="preconnect"
                         href="https://fonts.gstatic.com"
-                        crossorigin
+                        crossOrigin="true"
                     />
                     <link
                         href="https://fonts.googleapis.com/css2?family=Radio+Canada&family=Titan+One&display=swap"
