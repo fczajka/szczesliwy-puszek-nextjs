300b74cc09b1c5e39f7b2aa1253aa71eb52ac860
FranciszekCzajka <franekczajkaa@gmail.com>
2022-08-04 18:32:35 +0200

    Change styling in flex - width -> basis

--- components/Navbar.js
+++ components/Navbar.js
@@ -24,14 +24,14 @@ const Navbar = () => {
     };
 
     return (
-        <div className="w-full h-60px flex items-center justify-center relative text-babyBlue-1500 shadow-md xsm:h-80px">
-            <div className="w-full h-full max-w-1920 flex">
-                <div className="w-full text-3xl flex items-center justify-center font-titan-one xsm:text-4xl lg:w-7/12">
+        <div className="w-full h-60px justify-center relative text-babyBlue-1500 shadow-md xsm:h-80px">
+            <div className="w-full h-full max-w-1920 flex my-0 mx-auto">
+                <div className="w-full text-3xl flex items-center justify-center font-titan-one xsm:text-4xl lg:basis-7/12">
                     <Link href="/">
                         <a>{layout.header.title}</a>
                     </Link>
                 </div>
-                <ul className="hidden w-0 h-full justify-center items-center font-radio-canada space-x-4 lg:w-5/12 lg:flex">
+                <ul className="hidden w-0 h-full justify-center items-center font-radio-canada space-x-4 lg:flex lg:basis-5/12">
                     {layout.header.menu.map((listElement) => (
                         <li key={listElement.text}>
                             {listElement.array ? (
