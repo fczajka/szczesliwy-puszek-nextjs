4ba3339d6452f2061bd8c79ebeec4aa8c96f09f2
fczajka <franekczajkaa@gmail.com>
2022-08-27 14:29:19 +0200

    Fix styling in mobile navbar and 'Why us' section

--- components/WhyUs.js
+++ components/WhyUs.js
@@ -3,15 +3,15 @@ import { whyUs } from "../public/content";
 
 const WhyUs = () => {
     return (
-        <div className="w-full flex flex-col items-center pb-10 bg-why-us bg-no-repeat md:px-20 2xl:px-28">
-            <h2 className="my-16 font-titan-one text-royalPink-1500 text-3xl sm:text-4xl">
+        <div className="w-full flex flex-col items-center bg-why-us bg-no-repeat md:px-20 2xl:px-28">
+            <h2 className="my-16 mb-4 font-titan-one text-royalPink-1500 text-3xl sm:text-4xl">
                 {whyUs.title}
             </h2>
             <div className="w-full flex items-center flex-col xl:flex-row xl:justify-evenly">
                 {whyUs.cards.map((column) => (
                     <div
                         key={column.side}
-                        className="lg:w-full lg:flex lg:flex-row lg:justify-around"
+                        className="md:w-full md:flex md:flex-row md:justify-evenly xl:justify-around"
                     >
                         {column.columns.map((card) => (
                             <WhyUsCards
