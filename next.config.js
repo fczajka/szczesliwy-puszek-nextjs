5afd0a1448df2a7f6f821a1e3729f2bf38073fe5
fczajka <franekczajkaa@gmail.com>
2022-09-02 23:44:10 +0200

    Add environment variables

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        SERVICE_ID: "gmail",
+        TEMPLATE_ID: "template_7y5yjvg",
+        PUBLIC_KEY: "d-iXMGfxsUvFFZHe0",
+    },
     reactStrictMode: true,
     swcMinify: true,
     i18n: {
