9e7e6aaba38770740fba9408e3bb50d7c2dd170b
fczajka <franekczajkaa@gmail.com>
2022-09-26 22:02:21 +0200

    Fix bug with small gallery

--- components/Layout.js
+++ components/Layout.js
@@ -7,14 +7,14 @@ import Transition from "../components/Transition";
 
 const Layout = ({ children }) => {
     return (
-        <div className="bg-babyBlue-0 w-full flex flex-col items-center text-babyBlue-1500">
-            <Navbar content={navbar} />
-            <Transition>
+        <Transition>
+            <div className="bg-babyBlue-0 w-full flex flex-col items-center text-babyBlue-1500">
+                <Navbar content={navbar} />
                 <div className="max-w-1920 w-full">{children}</div>
                 <Contact content={contact} />
-            </Transition>
-            <MobileNavbar content={mobileNavbar} />
-        </div>
+                <MobileNavbar content={mobileNavbar} />
+            </div>
+        </Transition>
     );
 };
 
